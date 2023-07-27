270ddf4e68700b9cefeb6657158172acefa8cfd8
robertobenfini <roberto.benfini@gmail.com>
2023-07-27 10:12:28 +0200

    [FIXED] cambiato-parametro-colonna-in-time

---
+++ database/migrations/2023_07_27_075329_update_trains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->time('orario_di_partenza')->nullable()->default(NULL)->change();
+            $table->time('orario_di_arrivo')->nullable()->default(NULL)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            //
+        });
+    }
+};
